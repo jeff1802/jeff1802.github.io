37bdf1b242e2f317b9fc0df7b08f2e7bfe1c5615
Jeff Reding <den_jeff@hotmail.com>
2018-05-29 15:52:28 +0200

    json datei hinzugefügt

--- biketirol/etappe24.js
+++ biketirol/etappe24.js
@@ -4,6 +4,8 @@ let myMap = L.map("map",{
    });
 const RouteGroup=L.featureGroup().addTo(myMap);
 const routemarkerGroup=L.featureGroup().addTo(myMap);
+let overlaySteigung = L.featureGroup().addTo(myMap);
+
 let myLayers = {
 
     osm : L.tileLayer
@@ -44,7 +46,8 @@ let myMapControl= L.control.layers({
     "Orthophoto":myLayers.eKarte_ortho
 },
 {"Route": RouteGroup,
-"Start/Ziel": routemarkerGroup},
+"Start/Ziel": routemarkerGroup,
+"Steigungslinie": overlaySteigung},
 {
     collapsed:false
 }
@@ -94,6 +97,17 @@ gpxTrack.on('loaded',function(evt){
     myMap.fitBounds(evt.target.getBounds());
 });
 
+gpxTrack.on('addline', function(evt){
+    hoehenprofil.addData(evt.line);
+    console.log(evt.line);
+    console.log(evt.line.getLatLngs())
+    console.log(evt.line.getLatLngs()[0])
+    console.log(evt.line.getLatLngs()[0].meta)
+    console.log(evt.line.getLatLngs()[0].lat)
+    console.log(evt.line.getLatLngs()[0].lng)
+    console.log(evt.line.getLatLngs()[0].meta.ele);
+
+});
 //myMap.addLayer(RouteGroup);
 
 L.marker([47.227466,11.375286], {icon: L.icon({
@@ -113,3 +127,103 @@ L.marker([47.213184,11.213361], {icon: L.icon({
 //routemarkerGroup.addTo(myMap);
 
 //myMap.fitBounds(routemarkerGroup.getBounds());
+
+//elevation hinzufügen 
+
+//all used options are the default values
+
+//Höhenprofil control
+
+var hoehenprofil= L.control.elevation({
+    position: "topright",
+    theme: "steelblue-theme",
+    collapsed: true
+}).addTo(myMap);
+
+
+
+/*var el = L.control.elevation({
+    position: "topright",
+  theme: "steelblue-theme", //default: lime-theme
+  width: 600,
+  height: 125,
+  margins: {
+      top: 10,
+      right: 20,
+      bottom: 30,
+      left: 50
+  },
+  useHeightIndicator: true, //if false a marker is drawn at map position
+  interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+  hoverNumber: {
+      decimalsX: 3, //decimals on distance (always in km)
+      decimalsY: 0, //deciamls on hehttps://www.npmjs.com/package/leaflet.coordinatesight (always in m)
+      formatter: undefined //custom formatter function may be injected
+  },
+  xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+  yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+  collapsed: false,  //collapsed mode, show chart on click or mouseover
+  imperial: false    //display imperial units instead of metric
+});
+el.addTo(myMap);
+L.geoJson(geojson,{
+  onEachFeature: el.RouteGroup.bind(el) //working on a better solution
+}).addTo(myMap);
+
+var el = L.control.elevation();
+el.addTo(myMap);
+var g=new L.GPX("../etappe24.gpx", {async: true});
+g.on("addline",function(e){
+	el.RouteGroup(e.line);
+});
+g.addTo(myMap);*/
+
+//alle Segmente der Steigungslinie hinzufügen 
+
+var segment = L.polyline(
+    [
+    [p2.lat,p2.lng],
+    [p1.lat,p1.lng],
+    ],
+    {
+        color: farbe,
+        weight: 5
+    }
+).addTo(myMap);
+
+let gpxLinie = evt.line.getLatLngs();
+for (let i = 1; i < gpxLinie.lenght; i++) {
+    let p1 = gpxLinie[i-1];
+    let p2 = gpxLinie[i];
+    console.log(p1.lat.lng,p2.lat,p2.lng);
+
+}
+//Entfernung zwischen den Punkten berechnen
+let dist = myMap.distance(
+    [p2.lat,p2.lng],
+    [p1.lat,p1.lng],
+)
+
+//Höhenunterschied berechnen
+let delta = p2.meta.ele - p1.meta.ele;
+
+//Steigung in % berechnen
+
+let proz = (dist>0) ? (delta / dist * 100.0).toFixed(1):0
+
+console.log(p1.lat,p1.lng,p2.lat,p2.lng,dist,delta,proz);
+
+// grün ['#edf8e9','#bae4b3','#74c476','#238b45']
+//rot ['#fee5d9','#fcae91','#fb6a4a','#cb181d']
+
+let farbe = 
+    proz>15 ?'#cb181d':
+    proz>10 ?'#fb6a4a':
+    proz>6  ?'#fcae91':
+    proz>2  ?'#fee5d9':
+    proz>0 ?'#969696':
+    proz>-2 ?"#edf8e9":
+    proz>-6 ?'#bae4b3':
+    proz>-10?'#74c476':
+    proz>-15?'#238b45':
+            "green";
